d966f76a7eecbb4957150e6e82a4c9f9c0a34906
mugywarano <129194529+mugywarano@users.noreply.github.com>
2024-08-20 07:52:42 -0300

    Add files via upload

--- projeto novo/main.js
+++ projeto novo/main.js
@@ -10,10 +10,12 @@ const perguntas = [
     alternativas:[
         {
             texto: "A) Agachamento",
+            afirmacao: "Afirmação 1a alternativa"
         },
         {
             texto: "B) leg press",
-            
+            afirmacao: "Afirmação 2a alternativa"
+           
         },
     ]
 },
@@ -23,10 +25,12 @@ const perguntas = [
     alternativas:[
         {
             texto: "A)Focar apenas em exercícios de força, como levantamento terra",
+            afirmacao: "Afirmação 1a alternativa"
            
         },
         {
             texto: "B) Combinar exercícios de força com exercícios de isolamento, como extensões de perna",
+            afirmacao: "Afirmação 2a alternativa"
            
         },
     ]
@@ -36,13 +40,13 @@ const perguntas = [
     enunciado: "Qual é a principal vantagem de incluir o leg press na rotina de treino de pernas?",
     alternativas:[
         {
-            texto: "A) Trabalha principalmente os músculos da panturrilha"
-            
-            
+            texto: "A) Trabalha principalmente os músculos da panturrilha",
+            afirmacao: "Afirmação 1a alternativa"          
            
         },
         {
             texto: "B) Permite carregar mais peso e aumentar a força dos quadríceps e glúteos",
+            afirmacao: "Afirmação 2a alternativa"
            
         },
     ]
@@ -53,13 +57,36 @@ const perguntas = [
     alternativas:[
         {
             texto: "A) Aumenta a flexibilidade e reduz o risco de lesões",
-            
+            afirmacao: "Afirmação 1a alternativa"
+           
         },
         {
             texto: "B) Aumenta a massa muscular diretamente",
-          
+            afirmacao: "Afirmação 2a alternativa"
+         
         },
     ]
 },
 
 ]
+
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    caixaAlternativas.textContent="";
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntas[atual].enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        });        
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
